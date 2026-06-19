bb41e0f16ab66b6e808699893927828b28831eec
burakkepuc <burakkepuc.1996@gmail.com>
2026-06-20 01:07:22 +0300

    CalculateMachine program added

---
+++ TemelKavramlar/untitled/src/Main.java
@@ -0,0 +1,89 @@
+public class Main {
+    public static void main(String[] args) {
+//        int a = 10, b = 10, c;
+//        c = a == b ? 1 : 2;
+//        System.out.println(c);
+//        boolean kosul1 = (a < b);
+//        boolean kosul2 = (a < c );
+//        boolean sonuc = kosul1 || kosul2;
+//
+//        System.out.println(sonuc);
+
+// Koşullu ifadeler
+        // IF - ELSE
+
+//        int a = 10, b = 20;
+//        boolean compare = a == b;
+//        String str = compare ? "Eşittir" : "Eşit değildir.";
+//
+//        System.out.println(str);
+//
+//        if(compare){
+//            System.out.println("Eşittir");
+//        }else{
+//            System.out.println("Eşit değildir");
+//        }
+//        System.out.println("Program bitti.");
+
+        // En küçük olanı bul.
+//        int a = 10,b=10,c=-10;
+//        if(a < b && a < c){
+//            System.out.println("En küçük a: " + a);
+//        }
+//        else if(b<a && b<c){
+//            System.out.println("En küçük b: " + b);
+//        }else if(c<a && c<b){
+//            System.out.println("En küçük c: " + c);
+//        }
+//        else{
+//            System.out.println("Hepsi birbirine eşit");
+//        }
+//    }
+//}
+//        int a = 20, b = 10;
+//if ((a < b) && (b++ < 25))
+//{
+//    System.out.println(a);
+//}else{
+//    System.out.print(a); }
+//
+//System.out.println(b);
+
+        // Switch case
+        // Eşitliği kontrol eder. Büyük küçük işaretini vs kontrol etmez.
+
+//        int day = 10;
+//
+//        switch (day) {
+//            case 1:
+//                System.out.println("Pazartesi");
+//                break;
+//
+//            case 2:
+//                System.out.println("Salı");
+//                break;
+//            case 3:
+//                System.out.println("Çarşamba");
+//                break;
+//            case 4:
+//                System.out.println("Perşembe");
+//                break;
+//            case 5:
+//                System.out.println("Cuma");
+//                break;
+//            case 6:
+//                System.out.println("Cumartesi");
+//                break;
+//            case 7:
+//                System.out.println("Pazar");
+//                break;
+//            default:
+//                System.out.println("Yanlış giriş");
+//                break;
+//        }
+//        Eğer tek bir değişkenin farklı sabit değerlere göre dallanmasını istiyorsan switch daha okunaklıdır.
+//        Koşulların karşılaştırma operatörleri veya birden fazla mantıksal ifade içeriyorsa if-else doğru seçimdir.
+
+    }
+
+}
