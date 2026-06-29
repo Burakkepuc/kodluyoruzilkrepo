d984a410391cfb26e5c27df93f5c37cc27232e5c
burakkepuc <burakkepuc.1996@gmail.com>
2026-06-29 23:05:14 +0300

    find min and max number

---
+++ TemelKavramlar/untitled/src/MaxAndMinFind.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class MaxAndMinFind {
+   public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int adet;
+        int sayi,minsayi=Integer.MAX_VALUE,maxsayi=Integer.MIN_VALUE;
+
+
+        System.out.println("Kaç tane sayı gireceksiniz ?");
+        adet = scan.nextInt();
+
+        if(adet <= 0){
+            System.out.println("Geçerli bir sayı giriniz.");
+            return;
+        }
+
+        int i = 0;
+        while(adet != i){
+            System.out.print(i+1 +". Sayıyı giriniz: ");
+            sayi = scan.nextInt();
+
+            if(sayi<minsayi){
+                 minsayi = sayi;
+            }
+            if(sayi>maxsayi){
+                maxsayi = sayi;
+            }
+
+            i++;
+
+        }
+
+        System.out.println("Minimum: " + minsayi);
+        System.out.println("Maximum: " + maxsayi);
+
+
+    }
+}
