02cf92650f08224360356e40a0726d1e84501165
burakkepuc <burakkepuc.1996@gmail.com>
2026-07-07 01:21:20 +0300

    recursive design program added

---
+++ TemelKavramlar/untitled/src/RecursiveDesign.java
@@ -0,0 +1,21 @@
+public class RecursiveDesign {
+
+    static int f(int num){
+        System.out.println(num);
+
+        if(num < 0)
+            return num;
+         f(num - 5);
+
+//Java'da bir metot çağrısı bitmeden altındaki satıra asla geçilmez
+
+        System.out.println(num);
+
+         return num;
+    }
+
+
+    static void main(String[] args) {
+        f(16);
+    }
+}
